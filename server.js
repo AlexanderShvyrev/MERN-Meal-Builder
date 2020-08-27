345dd96b8d0980ba7db43d78f40cf72b118c9c99
Alexander Shvyrev <shurik_stv@yahoo.com>
2020-08-27 12:55:08 -0400

    please workk

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ require('./server/routes/user.routes')(app)
 
 //Serve static assets
 if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'))
+    // Set static folder
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(port, () => console.log(`Server has connected successfully ++++++++++++++ on port: ${port}`));
